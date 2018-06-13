0e50ec4bc6b704d127e488016a9bca34ced1fab0
Michelle Seyler <michelle.m.seyler@gmail.com>
2018-06-13 20:24:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 var candyString = 'foo'
 function addElementToBeginningOfArray(chocolateBars, candyString) {
- return chocolateBars = ['candyString', ... chocolateBars] 
+ return chocolateBars = [candyString, ... chocolateBars] 
 }
 
